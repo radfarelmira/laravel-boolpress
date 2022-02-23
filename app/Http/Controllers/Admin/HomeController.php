faf4040f01210bb309537ceaae674b0152bb4a63
radfarelmira <radfar.elmira@gmail.com>
2022-02-23 14:56:38 +0100

    create userinfo table and relation with user table

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,14 +5,17 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\UserInfo;
 
 class HomeController extends Controller
 {
     public function index() {
         $user = Auth::user();
-        
+    
         $id = Auth::id();
 
-        return view('admin.home', compact('user', 'id'));
+        $userInfo = $user->userInfo;
+
+        return view('admin.home', compact('user', 'id', 'userInfo'));
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <h1>Hi {{ $user->name }} {{ $id }}! Welcome to your Dashboard</h1>
+    <h1>Ciao {{ $user->name }} {{ $id }}! Benvenuto nel tuo Dashboard</h1>
+
+    @if ($userInfo)
+        <div>Telefono: {{ $userInfo->phone}}</div>
+        <div>Indirizzo: {{ $userInfo->address}}</div>
+    @endif
 @endsection
