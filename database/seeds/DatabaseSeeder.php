39c4aaf07ca0c6b61d312acd0f79725898fecb31
radfarelmira <radfar.elmira@gmail.com>
2022-02-25 14:57:54 +0100

    created e populated tags tabe and seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UserSeeder::class);
         $this->call(PostsTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
+        $this->call(TagsTableSeeder::class);
     }
 }
